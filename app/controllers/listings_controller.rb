2af0ca39621f2a16e9eed1c5a6498e6577e30b21
snith nhotprasa <lesnith@gmail.com>
2024-06-18 19:40:00 +0200

    first commit of the training exercise for the certif

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,38 @@
+class ListingsController < ApplicationController
+
+  def index
+    @listings = Listing.all
+    render json: { listings: @listings.as_json(except: [:created_at, :updated_at]) }
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+    render json: {listing: @listing.as_json(except: [:created_at, :updated_at])}
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.save
+    render json: @listing, status: :created
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params)
+    render json: @listing
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:num_room)
+  end
+end
+
+# created_at
+# updated_at
